7294966efc15d7e7d062dffa4ec817f203364240
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-12 22:47:26 +0700

    folder 13.1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import Navbar from './layouts/NavbarAndFooter/Navbar';
 import './App.css';
 import Footer from './layouts/NavbarAndFooter/Footer';
 import HomePage from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </>
   );
