9d2283e84809ff8bd73f3013b530a2a95d75d548
Harvey Nguyen <74479558+quocnhat02@users.noreply.github.com>
2023-06-18 20:03:05 +0700

    Update CheckoutAndReviewBox.tsx folder 21.4

--- client/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ client/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -6,7 +6,38 @@ const CheckoutAndReviewBox: React.FC<{
   book: BookModel | undefined;
   mobile: boolean;
   currentLoansCount: number;
+  isAuthenticated: any;
+  isCheckedOut: boolean;
+  checkoutBook: any;
 }> = (props) => {
+  function buttonRender() {
+    if (props.isAuthenticated) {
+      if (!props.isCheckedOut && props.currentLoansCount < 5) {
+        return (
+          <button
+            onClick={() => props.checkoutBook()}
+            className='btn btn-success btn-lg'
+          >
+            Checkout
+          </button>
+        );
+      } else if (props.isCheckedOut) {
+        return (
+          <p>
+            <b>Book checked out. Enjoy!</b>
+          </p>
+        );
+      } else if (!props.isCheckedOut) {
+        return <p className='text-danger'>Too many books checked out.</p>;
+      }
+    }
+    return (
+      <Link to={'/login'} className='btn btn-success btn-lg'>
+        Sign in
+      </Link>
+    );
+  }
+
   return (
     <div
       className={
@@ -38,9 +69,7 @@ const CheckoutAndReviewBox: React.FC<{
             </p>
           </div>
         </div>
-        <Link to='#' className='btn btn-success btn-lg'>
-          Sign In
-        </Link>
+        {buttonRender()}
         <hr />
         <p className='mt-3'>
           This number can change until placing order has been complete.
