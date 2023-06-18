114c80b16803b3ba5e9d27f12bd12892f82d5b11
Harvey Nguyen <74479558+quocnhat02@users.noreply.github.com>
2023-06-18 16:51:07 +0700

    Update ReturnBook.tsx folder 18.2

--- client/src/layouts/HomePage/components/ReturnBook.tsx
+++ client/src/layouts/HomePage/components/ReturnBook.tsx
@@ -6,7 +6,16 @@ import { Link, NavLink } from 'react-router-dom';
 const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-      <div className='text-center'>
+      <div
+        className='text-center'
+        style={{
+          height: '400px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
         {props.book.img ? (
           <img src={props.book.img} alt='book' width={'151'} height={'233'} />
         ) : (
@@ -17,9 +26,12 @@ const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
             height={'233'}
           />
         )}
-        <h6 className='mt-2'>{props.book.title}</h6>
-        <p>{props.book.author}</p>
-        <Link to={`/checkout/${props.book?.id}`} className='btn main-color text-white'>
+        <h6 className='mt-2'>{props.book?.title}</h6>
+        <p>{props.book?.author}</p>
+        <Link
+          to={`/checkout/${props.book?.id}`}
+          className='btn main-color text-white'
+        >
           Reserve
         </Link>
       </div>
