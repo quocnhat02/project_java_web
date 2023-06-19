8dbd10bae7206c2229c9942e0ac89e14c2621814
Chau Trong Nhan <61001185+ctn2002@users.noreply.github.com>
2023-06-19 22:59:14 +0700

    modify security

--- server/booksapp/src/main/java/com/nhom17books/booksapp/config/SecurityConfiguration.java
+++ server/booksapp/src/main/java/com/nhom17books/booksapp/config/SecurityConfiguration.java
@@ -19,7 +19,8 @@ public class SecurityConfiguration {
                                 .antMatchers("/api/books/secure/**",
                                         "/api/reviews/secure/**",
                                         "/api/messages/secure/**",
-                                        "/api/admin/secure/**")
+                                        "/api/admin/secure/**",
+                                        "/api/orders/secure/**")
                                 .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
